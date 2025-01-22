de77b34e72f516b44506b9676082f9c0701ee42a
ZiadAladawySE <zeyad20225162fcihelwanedueg@gmail.com>
2025-01-23 01:56:43 +0200

    debugging: the rest of the machine controller modules revised

--- server/Controllers/Machine/editMachine.js
+++ server/Controllers/Machine/editMachine.js
@@ -1,41 +1,41 @@
-const express = require('express'); 
-const { error } = require('console');
+const express = require('express');
+const { error, log } = require('console');
 const bodyParser = require('body-parser');
-const { query ,  pool} = require('../../config/data_base'); 
+const { query, pool } = require('../../config/data_base');
 
-const editMachine = async(req , res)=>{
+const editMachine = async (req, res) => {
+    let { id, name, latitude, longitude, state } = req.body;
 
-    const { id , name , latitude , longitude , state } = req.body;
-    const machine = (await query(`select * from machine where id = ${id}`))[0];
-    if(!machine) return res.status(404).json({message : "machine with this id not found !"})
+    try {
+        const machine = (await query(`SELECT * FROM machine WHERE id = ${id}`))[0];
+        if (!machine) return res.status(404).json({ message: "Machine with this ID not found!" });
 
-    if(!!latitude ^ !!longitude){
-        if(!latitude) latitude = machine.latitude ;
-        else longitude = machine.longitude ;
-    }
+        if (!!latitude ^ !!longitude) {
+            if (!latitude) latitude = machine.latitude;
+            else longitude = machine.longitude;
+        }
 
-    const found  = (await query(`select * from machine 
-        where 
-        (name = '${name}' or (latitude = ${latitude} and longitude = ${longitude})) and id != ${id}`))[0]
-        
-    if(found) {
-      if(found.name == name )  return res.status(401).json({message : "cannot update the name!"});
-      if(found.latitude == latitude && found.longitude == longitude )  return res.status(401).json({message : "cannot update the location!"});
-    }
+        const found = (await query(`SELECT * FROM machine 
+            WHERE 
+            (name = '${name}' OR (latitude = ${latitude} AND longitude = ${longitude})) AND id != ${id}`))[0];
 
-    if(!name ) name = machine.name ;
-    if(!state) state = machine.state ;
+        if (found) {
+            if (found.name === name) return res.status(400).json({ message: "Cannot update the name!" });
+            if (found.latitude === latitude && found.longitude === longitude) return res.status(400).json({ message: "Cannot update the location!" });
+        }
 
-    try{
-        await query(`update machine set 
-            name = '${name}' , longitude = '${longitude}' , latitude = '${latitude}' , state = '${state}'
-            where id = ${id}`);
+        if (!name) name = machine.name;
+        if (!state) state = machine.state;
 
-        return res.status(200).json({message : "updated successfully"});
-    }
-    catch(err){
-        return res.status(500).json({message : err });
+        await query(`UPDATE machine SET 
+            name = '${name}', longitude = ${longitude}, latitude = ${latitude}, state = '${state}'
+            WHERE id = ${id}`);
+
+        return res.status(200).json({ message: "Updated successfully" });
+    } catch (err) {
+        log(err); // Log the error for debugging
+        return res.status(500).json({ message: "Internal server error" });
     }
 }
 
-module.exports = editMachine ; 
+module.exports = editMachine;
--- server/Controllers/Machine/getAllMachines.js
+++ server/Controllers/Machine/getAllMachines.js
@@ -1,20 +1,16 @@
-const express = require('express'); 
-const { error } = require('console');
+const express = require('express');
+const { error, log } = require('console');
 const bodyParser = require('body-parser');
-const { query ,  pool} = require('../../config/data_base'); 
+const { query, pool } = require('../../config/data_base');
 
-
-const GetAllMachines = async(req , res)=>{
-    
-    try{
-        const Machines = await query(`select * from machine`) ;
-        
-        // if(!Machines[0]) return res.status(404).json({message :"No machine has been added yet "});
-
-        return res.status(200).json({message : "Done" , result : Machines});
-    }
-    catch(err){
-        return res.status(500).json({message : err});
+const GetAllMachines = async (req, res) => {
+    try {
+        const Machines = await query(`SELECT * FROM machine`);
+        return res.status(200).json({ message: "Done", result: Machines });
+    } catch (err) {
+        log(err); //error checking
+        return res.status(500).json({ message: "Internal server error" });
     }
 }
-module.exports = GetAllMachines ;
+
+module.exports = GetAllMachines;
